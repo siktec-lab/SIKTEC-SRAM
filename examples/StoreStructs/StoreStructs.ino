d733d1be66f28bd578241af053d8860fb92548ce
Shlomo Hassid <shlomohassid@gmail.com>
2022-05-07 14:49:58 +0300

    Added struct serialization example

--- examples/StoreStructs/StoreStructs.ino
+++ examples/StoreStructs/StoreStructs.ino
@@ -5,7 +5,8 @@
 // Copyright 2022, SIKTEC.
 /******************************************************************************/
 /*****************************      DESCRIPTION       *************************
- * 
+ * A simple example on how to declare the SRAM object, and directly writing 
+ * and reading structs.
 /*****************************      NOTES       *******************************
     -> Tested with 23K256-I/SN - should work with others too.
     -> datasheet of test chip: 
@@ -19,47 +20,82 @@
 #include <Arduino.h>
 #include <SIKTEC_SRAM.h>
 
-#define SRAM_CS     17
+//Define SRAM CS Pin attached:
+#define SRAM_CS 17
 
+using namespace SIKtec;
+
+//Declare SRAM object:
 SIKTEC_SRAM sram(SRAM_CS);
+
+//Example struct we are going to serialize and write to SRAM:
+typedef struct MyData {
+    int count;
+    uint8_t mode;
+    char name[10];
+    void print() {
+        Serial.print("Name -> ");
+        Serial.println(name);
+        Serial.print("Mode -> ");
+        Serial.println(mode);
+        Serial.print("Count -> ");
+        Serial.println(count);
+    }
+} Mydata_t;
+
+//Helper function that prints the SRAM mode an
+void print_sram_mode();
 
 void setup() {
     
     Serial.begin(115200);
     while (!Serial) { delay(10); }
 
-    Serial.println("Sram interfacing Example 1");
+    Serial.println("\nSRAM Struct Example:\n");
 
-    //Start Sram communication
+    //Start SRAM Communication
     sram.begin();
 
-    //Put in seq mode:
+    //Put SRAM in sequential mode:
     if (!sram.set_mode(SRAM_MODE::SRAM_SEQ_MODE)) {
         Serial.print("SRAM MODE FAILED");
     } else {
         print_sram_mode();
     }
 
+    //Test data - simple integer array:
+    Mydata_t data = {100, 8, "SRAM"};
+    
+    Serial.println("\nTest data print .... ");
+    data.print();
+
     Serial.println("\nWriting data to SRAM .... ");
-    uint8_t data[] = {1, 2, 3, 4, 5, 6, 7, 8};
-    uint16_t length = 8;
-    sram.write(0x0000, data, length);
+    uint8_t writeBuffer[sizeof(data)];
+    memcpy(writeBuffer, &data, sizeof(data));
+    sram.write(0x0000, writeBuffer, sizeof(writeBuffer));
 
-    Serial.println("SRAM DUMP first 10 bytes:");
-    sram.mem_dump(0x0000, 10);
+    //DUMP after write:
+    Serial.println("SRAM DUMP written bytes:");
+    sram.mem_dump(0x0000, sizeof(writeBuffer));
 
-    Serial.println("\nErasing data from SRAM [0x3 -> 0x5].... ");
-    sram.erase(0x0002, 3);
-
-    Serial.println("SRAM DUMP first 10 bytes:");
-    sram.mem_dump(0x0000, 10);
-
+    //Reading back the stored values:
+    Serial.println("\nReading back:");
+    Mydata_t back;
+    uint8_t readBuffer[sizeof(back)];
+    sram.read(0x0000, readBuffer, sizeof(readBuffer));
+    memcpy(&back, readBuffer, sizeof(readBuffer));
+    back.print();
 }
 
 void loop() {
     delay(500);
 }
 
+/**
+ * @brief will print the current SRAM mode and the status register
+ * 
+ * @return void
+ */
 void print_sram_mode() {
     Serial.print("SRAM IS IN MODE => ");
     if (sram.is_mode(SRAM_MODE::SRAM_SEQ_MODE)) {
